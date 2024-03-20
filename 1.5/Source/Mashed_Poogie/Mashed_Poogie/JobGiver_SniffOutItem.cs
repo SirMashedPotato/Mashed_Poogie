9e7cc786c81395a0367ef458bb099c8c6cb6f4cc
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-03-20 17:20:27 +1300

    Update to 1.0.2 - 1.5 Support

---
+++ 1.5/Source/Mashed_Poogie/Mashed_Poogie/JobGiver_SniffOutItem.cs
@@ -0,0 +1,84 @@
+﻿using Verse;
+using Verse.AI;
+using RimWorld;
+
+namespace Mashed_Poogie
+{
+    public class JobGiver_SniffOutItem : ThinkNode_JobGiver
+    {
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            if (pawn.Downed)
+            {
+                return null;
+            }
+			IntVec3 c = TryFindDigCell(pawn);
+			if (!c.IsValid)
+			{
+				return null;
+			}
+			return JobMaker.MakeJob(JobDefOf.Mashed_Poogie_SniffOutItem, c);
+		}
+
+        private static IntVec3 TryFindDigCell(Pawn pawn)
+        {
+            if (!CellFinder.TryFindClosestRegionWith(pawn.GetRegion(RegionType.Set_Passable), TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false, false, false), (Region r) => r.Room.PsychologicallyOutdoors, 100, out Region rootReg, RegionType.Set_Passable))
+            {
+                return IntVec3.Invalid;
+            }
+            IntVec3 result = IntVec3.Invalid;
+            RegionTraverser.BreadthFirstTraverse(rootReg, (Region from, Region r) => r.District == rootReg.District, delegate (Region r)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    IntVec3 randomCell = r.RandomCell;
+                    if (IsGoodDigCell(randomCell, pawn))
+                    {
+                        result = randomCell;
+                        return true;
+                    }
+                }
+                return false;
+            }, 30, RegionType.Set_Passable);
+            return result;
+        }
+
+		private static bool IsGoodDigCell(IntVec3 c, Pawn pawn)
+		{
+            if (c.GetTerrain(pawn.Map).categoryType != TerrainDef.TerrainCategoryType.Soil &&
+            c.GetTerrain(pawn.Map).categoryType != TerrainDef.TerrainCategoryType.Sand)
+			{
+				return false;
+			}
+            if (!c.GetThingList(pawn.Map).NullOrEmpty())
+			{
+				return false;
+			}
+            if (c.IsForbidden(pawn))
+			{
+				return false;
+			}
+            if (c.Standable(pawn.Map))
+			{
+				return false;
+			}
+            for (int i = 0; i < 9; i++)
+			{
+				IntVec3 c2 = c + GenAdj.AdjacentCellsAndInside[i];
+				if (!c2.InBounds(pawn.Map))
+				{
+					return false;
+				}
+				if (!c2.Standable(pawn.Map))
+				{
+					return false;
+				}
+				if (pawn.Map.reservationManager.IsReservedAndRespected(c2, pawn))
+				{
+					return false;
+				}
+            }
+            return true;
+		}
+    }
+}
---
+++ 1.5/Source/Mashed_Poogie/Mashed_Poogie/ThinkNode_ConditionalBonded.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Mashed_Poogie
+{
+    public class ThinkNode_ConditionalBonded : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)
+        {
+            return !TrainableUtility.GetAllColonistBondsFor(pawn).EnumerableNullOrEmpty();
+        }
+    }
+}
