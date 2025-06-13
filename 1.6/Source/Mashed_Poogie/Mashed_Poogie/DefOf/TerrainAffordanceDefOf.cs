8dd9a67c109572c9c741c4930069f0896b5098d9
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-06-13 13:56:57 +1200

    1.6 update

---
+++ 1.6/Source/Mashed_Poogie/Mashed_Poogie/DefOf/TerrainAffordanceDefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Poogie
+{
+    [DefOf]
+    public static class TerrainAffordanceDefOf
+    {
+        static TerrainAffordanceDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(TerrainAffordanceDefOf));
+        }
+        public static TerrainAffordanceDef Diggable;
+    }
+}
